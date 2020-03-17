664088e6143f3c001ddc5d4505b0d56c23e21935
tzforevereer <1076889517@qq.com>
2020-03-17 09:24:41 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
+import TreeTable from 'vue-table-with-tree-grid'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -16,6 +17,7 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+Vue.component('tree-table', TreeTable)
 new Vue({
   router,
   render: h => h(App)
