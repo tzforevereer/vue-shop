76d6c3487f4cd74e811877c6b0dca7154edca2d3
tzforevereer <1076889517@qq.com>
2020-03-20 12:37:05 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,7 @@ import {
   Button, Form, FormItem, Input, Message, Container, Header, Aside, Main,
   Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination,
   Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Steps, Step, CheckboxGroup, Checkbox,
-  Upload
+  Upload, Timeline, TimelineItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -42,6 +42,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
